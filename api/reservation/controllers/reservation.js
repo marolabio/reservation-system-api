dde28478a936762c8b85da9748d40f3a0553918d
Omar Labio <omarlabio@localhost.localdomain>
2020-07-15 18:56:55 +0800

    initial create reservation request progress

--- api/reservation/controllers/reservation.js
+++ api/reservation/controllers/reservation.js
@@ -9,26 +9,39 @@ const { sanitizeEntity } = require("strapi-utils");
 
 module.exports = {
   async create(ctx) {
-    const { firstName, lastName, rooms } = ctx.request.body;
+    const { firstName, lastName, email, rooms } = ctx.request.body;
+    const checkUnavailableRoom = await strapi
+      .query("room")
+      .count({ available_lte: 0, id_in: rooms.map((room) => room.id) });
+
+    // Validations
     if (rooms.length < 0) throw new Error("Rooms are required.");
+    if (checkUnavailableRoom !== 0) throw new Error("Unable to reserve.");
 
-    const createdCustomer = await strapi.services.customer.create({
-      first_name: firstName,
-      last_name: lastName,
-    });
-
-    const updatedRooms = await Promise.all(
+    // Update room quantity
+    await Promise.all(
       rooms.map(async (room) => {
-        return await strapi.services.room.update(
-          { id: room.id },
-          { available: room.quantity }
-        );
+        const currentRoom = await strapi.services.room.findOne({ id: room.id });
+        currentRoom.available = -room.quantity;
+
+        return await strapi.services.room.update({ id: room.id }, currentRoom);
       })
     );
 
+    // Create customer
+    const createdCustomer = await strapi.services.customer.create({
+      first_name: firstName,
+      last_name: lastName,
+      email,
+    });
+
+    // Create reservation
     let entity = await strapi.services.reservation.create({
       customer: createdCustomer.id,
-      room: updatedRooms.map((room) => room.id),
+      reserved_room: rooms.map((room) => ({
+        room: room.id,
+        reserved_quantity: room.quantity,
+      })),
     });
 
     return sanitizeEntity(entity, { model: strapi.models.reservation });
