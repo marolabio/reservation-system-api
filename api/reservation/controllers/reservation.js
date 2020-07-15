da6ca1b6f92754a928bf17d9efc81f0a152fe3da
Omar Labio <omarlabio@localhost.localdomain>
2020-07-15 14:55:23 +0800

    initial commit

---
+++ api/reservation/controllers/reservation.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
+ * to customize this controller
+ */
+
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  async create(ctx) {
+    const { firstName, lastName, rooms } = ctx.request.body;
+    if (rooms.length < 0) throw new Error("Rooms are required.");
+
+    const createdCustomer = await strapi.services.customer.create({
+      first_name: firstName,
+      last_name: lastName,
+    });
+
+    const updatedRooms = await Promise.all(
+      rooms.map(async (room) => {
+        return await strapi.services.room.update(
+          { id: room.id },
+          { available: room.quantity }
+        );
+      })
+    );
+
+    let entity = await strapi.services.reservation.create({
+      customer: createdCustomer.id,
+      room: updatedRooms.map((room) => room.id),
+    });
+
+    return sanitizeEntity(entity, { model: strapi.models.reservation });
+  },
+};
